b0e17e662da4d03cfae2a4d5f52114bf86dd3bf7
Apon Palanuwech <palaa159@newschool.edu>
2013-07-28 15:55:31 -0400

    RaphaelJS fail
    move to kineticJS

--- js/queryData.js
+++ js/queryData.js
@@ -1,12 +1,42 @@
+var scrWidth = window.innerWidth,
+	scrHeight = window.innerHeight,
+	faceSize = scrWidth / 8;
+var paper = Raphael("faceRender", scrWidth, scrHeight);
+window.onload = function() {
+	bodyChange();
+};
+
+var faceData;
+var faceArray = [];
 socket.emit('req data');
 socket.on('req result', function(data) {
-	console.log(data);
-	$.each(data, function(index, value) {
-		$('#faceForClone')
-		.clone()
-		.attr('title', value.publicOk)
-		.attr('id', value.id)
-		.attr('src', 'https://singyourface.s3.amazonaws.com/images/' + value.id + '.png')
-		.appendTo('#faceGrid');
+	faceData = data;
+	console.log(faceData);
+	$.each(faceData, function(index, value) {
+		faceArray[index] = paper.image('https://singyourface.s3.amazonaws.com/images/' + value.id + '.png', index*faceSize, 0, faceSize, faceSize);
 	});
+	// add mouse event
+	$.each(faceArray, function(index, value) {
+		faceArray[index].mouseover(function(e){
+			// pixel8
+			var imageData = faceArray[index].getImageData();
+		});
+	});
+	
 });
+// Resizing handler
+
+function bodyChange() {
+	$('body').css({
+		'width': scrWidth,
+		'height': scrHeight
+	});
+	// translate facedata
+}
+window.addEventListener('resize', onResize, false);
+
+function onResize() {
+	scrWidth = window.innerWidth, scrHeight = window.innerHeight;
+	faceSize = scrWidth / 8;
+	bodyChange();
+}
